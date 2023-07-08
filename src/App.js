cbabd7a62c2e2c8d6e816eefd0d388d118344b37
MrityunjayNPandey <pandeymrityunjay796@gmail.com>
2023-07-09 03:04:10 +0530

    made page with button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
+import Components from "./Pages/Components";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/components" element={<Components />} />
       </Routes>
     </BrowserRouter>
   );
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return <div>home</div>;
+  const navigate = useNavigate();
+  const handleButtonClick = () => {
+    navigate("/components"); // Replace '/components' with the actual path of the desired
+  };
+
+  return (
+    <div>
+      <h1>Home </h1>
+      <button onClick={handleButtonClick}>Go to Another </button>
+    </div>
+  );
 };
 
 export default Home;
