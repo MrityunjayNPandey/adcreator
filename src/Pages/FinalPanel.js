a0b66c2b9717036ee18e0239fe95a21ce14fb9d6
MrityunjayNPandey <pandeymrityunjay796@gmail.com>
2023-07-15 12:08:14 +0530

    removed download completely

--- src/Pages/FinalPanel.js
+++ src/Pages/FinalPanel.js
@@ -116,21 +116,11 @@ const FinalPanel = () => {
   const classes = useStyles();
   const { finalPhoto } = useSelector((state) => state);
 
-  const handleDownload = () => {
-    const link = document.createElement("a");
-    link.href = finalPhoto;
-    link.download = "finalImage.jpg"; // Set the desired file name
-    link.target = "_blank";
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  };
-
   const dispatch = useDispatch();
 
   const handleSave = async () => {
     const imageFile = finalPhoto;
-    const maxSizeInBytes = 1; // Bytes
+    const maxSizeInBytes = 1000; 
     console.log(imageFile.length);
     if (imageFile.length > maxSizeInBytes) {
       const compressedImageDataURL = await compressImage(
@@ -157,13 +147,6 @@ const FinalPanel = () => {
             />
           </div>
         )}
-        {/* <Button
-          variant="contained"
-          className={classes.redButton}
-          onClick={handleDownload}
-        >
-          Download
-        </Button> */}
         <Button
           variant="contained"
           color="primary"
